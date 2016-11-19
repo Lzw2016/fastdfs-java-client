af1f91621d20f9540d1239eb9e471648fbb84063
Lzw2016 <1183409807@qq.com>
2016-11-19 23:49:40 +0800

    ..............

---
+++ src/test/java/org/cleverframe/fastdfs/Test.java
@@ -0,0 +1,29 @@
+package org.cleverframe.fastdfs;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * 作者：LiZW <br/>
+ * 创建时间：2016/11/19 23:38 <br/>
+ */
+public class Test {
+    /**
+     * 日志记录器
+     */
+    private final static Logger logger = LoggerFactory.getLogger(Test.class);
+
+    @org.junit.Test
+    public void test01() {
+        logger.info("232");
+        logger.info("232");
+        logger.info("232");
+        logger.info("232");
+        logger.info("232");
+        logger.info("232");
+        logger.info("232");
+        logger.info("232");
+        logger.info("232");
+        logger.info("232");
+    }
+}
